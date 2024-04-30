e9a7d86e6f7dd9a8b00572fd20660c870e04e5cb
GabyFel20 <gabyfel20@gmail.com>
2024-04-30 17:51:42 -0600

    Ejercicios de logica con JS

---
+++ 1_User profile.js
@@ -0,0 +1,1 @@
+//Write a program that prompts a user for their data: username, age, and a list of their favorite movies. Store the information and then showcase it in the console. Take note that the output for the films should add a small message like: 'The film {film} is one of my favorites'.
